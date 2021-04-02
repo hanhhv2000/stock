57ae1bba5fd46c7395521d31a29f07dbc4f844d7
hanhhv2000 <81600599+hanhhv2000@users.noreply.github.com>
2021-04-02 15:29:20 +0700

    Update p1.py

--- p1.py
+++ p1.py
@@ -285,6 +285,7 @@ def get_dandat(hose):
                                              name='Điểm ảnh hưởng âm', marker={'color': 'red'}, width=0.8))
     dandatplot.update_layout(title_text='Nhóm dẫn dắt thị trường', autosize=False,legend=dict(
         orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1))
+    dandatplot.update_layout(barmode='group', xaxis_tickangle=-45)  
 #     dandatplot.update_layout( title='Nhóm dẫn dắt thị trường',autosize=False, xaxis_tickfont_size=12, yaxis=dict( title='Điểm ảnh hưởng', titlefont_size=12,tickfont_size=12,),
 #     legend=dict(x=1, y=1.02, bgcolor='rgba(255, 255, 255, 0)', bordercolor='rgba(255, 255, 255, 0)'))  
     dandatplot.update_layout(font_family="Courier New",font_color="blue", title_font_family="Times New Roman",
