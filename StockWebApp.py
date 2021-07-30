39b4b37a873f3e48af084086b92613992eb6f24a
hanhhv2000 <81600599+hanhhv2000@users.noreply.github.com>
2021-07-30 16:40:33 +0700

    Update StockWebApp.py

--- StockWebApp.py
+++ StockWebApp.py
@@ -229,7 +229,7 @@ def get_condition(exportList,rs_df, stock):
             print(f"Could not gather data on {st}")
     # currentClose
     exportList = exportList.sort_values(by='CHỈ SỐ RS', ascending=False)
-    
+    exportList = exportList.loc[{4,6,13,19}]
     return exportList
 
 # -=======================END OF GET_CONDITION====================================================#
